d926bed751376be36eee81240ad6e6168b498910
kuba <kuba.pietrak.94@gmail.com>
2020-08-25 19:46:07 +0200

    plot test

--- KGCN/plot_nxgraphs.py
+++ KGCN/plot_nxgraphs.py
@@ -4,9 +4,9 @@ import os
 import sys
 from pathlib import Path
 PATH = os.getcwd() #+'\data\\'
-SAVEPATH = PATH + "/nx_500n2500/"
+SAVEPATH = PATH + "/data/nx_500n2500/"
 
-scn_idx = 741
+scn_idx = 8860
 graph = nx.read_gpickle(str(SAVEPATH) + "graph_" + str(scn_idx) + ".gpickle")
 
 new_graph = nx.Graph(graph)
