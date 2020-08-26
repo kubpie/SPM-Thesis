e4ae1aa39de32ed1b36a26bad45c95400a1d5af2
kuba <kuba.pietrak.94@gmail.com>
2020-08-26 17:55:45 +0200

    more graphs in 1000n2500 set

--- KGCN/kgcn_ssp.py
+++ KGCN/kgcn_ssp.py
@@ -52,9 +52,9 @@ ALLDATA = LoadData(DATAPATH)
 ALLDATA = FeatDuct(ALLDATA, Input_Only = True) #leave only model input
 PROCESSED_DATA = pd.read_csv(str(DATAPATH)+"/ducts_data.csv")
 
-KEYSPACE =  "kgcn_500n1000" #"kgcn500n2500"
+KEYSPACE =  "kgcn_500n2500" #"kgcn500n2500"
 URI = "localhost:48555"
-SAVEPATH = PATH + "/data/nx_500n2500_bias/" #/data/nx_500n1000/ #nx_500n2500
+SAVEPATH = PATH + "/data/nx_500n2500_biasbig/" #/data/nx_500n1000/ #nx_500n2500
 
 # DATA SELECTION FOR GRAKN TESTING
 from data_analysis import ClassImbalance
@@ -65,12 +65,12 @@ from data_analysis import ClassImbalance
 # === 2 classes of 2000 sample 500/2500 ==== 
 #data = ALLDATA
 data_select = ALLDATA[(ALLDATA.loc[:,'num_rays'] == 500) | (ALLDATA.loc[:,'num_rays'] == 2500)]
-data = UndersampleData(data_select, max_sample = 300)
+data = UndersampleData(data_select, max_sample = 500)
 #data = data[(data.loc[:,'num_rays']==500) | (data.loc[:31,'num_rays'] == 2500)]
-data = data[:330]
+data = data[:550]
 #data = data_select
 class_population = ClassImbalance(data, plot = True)
-#plt.show()
+plt.show()
 print(class_population)
 
 # Existing elements in the graph are those that pre-exist in the graph, and should be predicted to continue to exist
@@ -570,10 +570,10 @@ train_graphs, tr_ge_split, training_data, testing_data = prepare_data(session, d
 kgcn_vars = {
           'num_processing_steps_tr': 15, #13
           'num_processing_steps_ge': 15, #13
-          'num_training_iterations': 5000, #10000?
+          'num_training_iterations': 3000, #10000?
           'learning_rate': 1e-4, #down to even 1e-4
           'latent_size': 16, #MLP param 16
-          'num_layers': 2, #MLP param 2 (try deeper configs)
+          'num_layers': 4, #MLP param 2 (try deeper configs)
           'clip': 5, #gradient clipping 5
           'weighted': False, #loss function modification
           'log_every_epochs': 50, #logging of the results
