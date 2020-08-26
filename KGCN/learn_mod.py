4c979f60264f27a4eef0f4ecf9e2afb4b3371995
kuba <kuba.pietrak.94@gmail.com>
2020-08-26 15:16:06 +0200

    added avg grad to control clipping

--- KGCN/learn_mod.py
+++ KGCN/learn_mod.py
@@ -28,7 +28,7 @@ from pathlib import Path
 from feed_mod import create_placeholders, create_feed_dict, create_batches_from_input, make_all_runnable_in_session
 from loss_mod import loss_ops_preexisting_no_penalty, loss_ops_from_difference
 from kglib.kgcn.learn.metrics import existence_accuracy
-
+from average_gradients import calc_average_grad
 from graph_nets import utils_np
 from graph_nets.graphs import GraphsTuple
 
@@ -48,31 +48,6 @@ class KGCNLearner:
         self._model = model
         self._num_processing_steps_tr = num_processing_steps_tr
         self._num_processing_steps_ge = num_processing_steps_ge
-
-    def average_gradients(grads):
-        average_grads = []
-        for grad_and_vars in zip(*grads):
-            # Note that each grad_and_vars looks like the following:
-            #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
-            grads = []
-            for g, _ in grad_and_vars:
-                # Add 0 dimension to the gradients to represent the tower.
-                expanded_g = tf.expand_dims(g, 0)
-
-                # Append on a 'tower' dimension which we will average over below.
-                grads.append(expanded_g)
-
-        # Average over the 'tower' dimension.
-        grad = tf.concat(axis=0, values=grads)
-        grad = tf.reduce_mean(grad, 0)
-
-        # Keep in mind that the Variables are redundant because they are shared
-        # across towers. So .. we will just return the first tower's pointer to
-        # the Variable.
-        v = grad_and_vars[0][1]
-        grad_and_var = (grad, v)
-        average_grads.append(grad_and_var)
-        return average_grads
 
     def train(self,
                  tr_input_graphs,
@@ -133,7 +108,7 @@ class KGCNLearner:
         optimizer = tf.train.AdamOptimizer(learning_rate)
         gradients, variables = zip(*optimizer.compute_gradients(loss_op_tr))
         
-        average_grads = average_gradients(gradients) #my add
+        average_grads = calc_average_grad(gradients) #my add
         print(average_grads)
 
         for grad, var in zip(gradients, variables):
