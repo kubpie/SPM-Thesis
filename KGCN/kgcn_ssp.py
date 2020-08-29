66da2e45f445aa278c5e4191c1fdc5e6341cf4ef
kuba <kuba.pietrak.94@gmail.com>
2020-08-29 15:21:27 +0200

    prep for full data run

--- KGCN/kgcn_ssp.py
+++ KGCN/kgcn_ssp.py
@@ -74,22 +74,22 @@ ALLDATA = FeatDuct(ALLDATA, Input_Only = True) #leave only model input
 PROCESSED_DATA = pd.read_csv(str(DATAPATH)+"/ducts_data.csv")
 KEYSPACE =  "kgcn_schema_full" #"kgcn500n2500" #"ssp_schema_slope0"  #"sampled_ssp_schema_kgcn"
 URI = "localhost:48555"
-SAVEPATH = str(DATAPATH) + "/nx_500n2500_bias/" #nx_500n2500
+SAVEPATH = str(DATAPATH) + "/nx_fullschema/" #nx_500n2500
 
 ### DATA SELECTION FOR GRAKN TESTING
 
 #data = UndersampleData(ALLDATA, max_sample = 100)
 #data = UndersampleData(data, max_sample = 30) #at 30 you got 507 nx graphs created, howeve with NotDuct at this point
 # === 2 classes of 2000 sample 500/2500 ==== 
-#data = ALLDATA
-data_select = ALLDATA[(ALLDATA.loc[:,'num_rays'] == 500) | (ALLDATA.loc[:,'num_rays'] == 2500)]
+data = ALLDATA
+#data_select = ALLDATA[(ALLDATA.loc[:,'num_rays'] == 500) | (ALLDATA.loc[:,'num_rays'] == 2500)]
 #data = UndersampleData(data_select, max_sample = 2000)
 #data = data[(data.loc[:,'num_rays']==500) | (data.loc[:31,'num_rays'] == 2500)]
-data = data[:310]
-data = data_select
-class_population = ClassImbalance(data, plot = True)
+#data = data[:310]
+#data = data_select
+#class_population = ClassImbalance(data, plot = True)
 #plt.show()
-print(class_population)
+#print(class_population)
 
 # Existing elements in the graph are those that pre-exist in the graph, and should be predicted to continue to exist
 PREEXISTS = 0
@@ -568,9 +568,9 @@ train_graphs, tr_ge_split, training_data, testing_data = prepare_data(session, d
 #, val_graphs,  val_ge_split
 
 kgcn_vars = {
-          'num_processing_steps_tr': 10, #13
-          'num_processing_steps_ge': 10, #13
-          'num_training_iterations': 5000, #10000?
+          'num_processing_steps_tr': 3, #13
+          'num_processing_steps_ge': 3, #13
+          'num_training_iterations': 100, #10000?
           'learning_rate': 1e-4, #down to even 1e-4
           'latent_size': 16, #MLP param 16
           'num_layers': 4, #MLP param 2 (try deeper configs)
--- KGCN/pipeline_mod.py
+++ KGCN/pipeline_mod.py
@@ -124,7 +124,8 @@ def pipeline(graphs,
     #Turned off plotting to speed up the runs
     plot_across_training(*tr_info, output_file=f'{output_dir}/learning.png')
     plot_predictions(graphs[tr_ge_split:], test_values, num_processing_steps_ge, output_file=f'{output_dir}/graph.png')
-
+    plt.show()
+    
     logit_graphs = graphs_tuple_to_networkxs(test_values["outputs"][-1])
 
     indexed_ge_graphs = indexed_graphs[tr_ge_split:]
